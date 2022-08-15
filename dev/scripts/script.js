fdb466050124240d2c8db2c42731fd72d035d029
JedBorseth <jedborseth@gmail.com>
2022-08-15 14:03:12 -0700

    working carousel

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,8 @@
 // Write your JavaScript here...
+$(".slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 100,
+  fade: true,
+  prevArrow: '<button type="button" class="slick-prev"></button>',
+});
