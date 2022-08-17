b1c60665e539bde1ec7cf90d3e727c1cf3abfee4
JedBorseth <jedborseth@gmail.com>
2022-08-17 12:51:50 -0700

    finished all sliders added styles in medai querys for various things

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,7 +1,8 @@
-// Write your JavaScript here...
+// Functions for sliders
 $(".slider").slick({
   dots: true,
   infinite: true,
+  centerPadding: "100px",
   speed: 600,
   fade: true,
   dotsClass: "slick-dots",
@@ -12,6 +13,84 @@ $(".slider").slick({
   // autoplay: true,
   // autoplaySpeed: 5000,
 });
+
+function categorySlider() {
+  if (window.innerWidth >= 427) {
+    $(".category-carousel").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      prevArrow:
+        '<button type="button" class="slick-next image-button left"><i class="fa-solid fa-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-prev image-button right"><i class="fa-solid fa-angle-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1012,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 427,
+          settings: "unslick",
+        },
+      ],
+    });
+    $(".news").slick({
+      dots: false,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 2,
+      prevArrow:
+        '<button type="button" class="slick-next image-button left"><i class="fa-solid fa-angle-left"></i></button>',
+      nextArrow:
+        '<button type="button" class="slick-prev image-button right"><i class="fa-solid fa-angle-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 1012,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 427,
+          settings: "unslick",
+        },
+      ],
+    });
+  }
+}
+categorySlider();
+let categoryQuery = window.matchMedia("(min-width:427px)");
+categoryQuery.addEventListener("change", categorySlider);
 $(".this-is-ae").on("click", (e) => {
   console.log(e.target);
 
